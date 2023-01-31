e2db857f6b09f6b6d209da630033d8453c9f87e2
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2023-01-31 09:40:56 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -0,0 +1,10 @@
+import sys
+
+num = int(sys.stdin.readline())
+
+for i in range(1, num+1):
+    for j in range(num-i):
+        print(end=' ')
+    for k in range(1, i+1):
+        print("*", end='')
+    print()

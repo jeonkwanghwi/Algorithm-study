def7e7eff5b707cd30185f6f9b7c0f3467f05464
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2023-02-04 17:47:11 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 420 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
@@ -2,6 +2,6 @@ n = int(input())
 lst = [float(input()) for _ in range(n)]
 
 for i in range(1, n):
-    lst[i] = max(lst[i - 1] * lst[i],   lst[i]) # 이전것 * 지금 vs 지금
-
+    lst[i] = max(lst[i], lst[i]*lst[i-1])
+    
 print("{:.3f}".format(max(lst)))

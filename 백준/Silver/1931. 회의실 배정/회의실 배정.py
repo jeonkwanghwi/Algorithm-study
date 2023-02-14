fdc5d525885b1a782d34ad33440084df05881bbe
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2023-02-15 08:55:41 +0900

    [Silver I] Title: 회의실 배정, Time: 4044 ms, Memory: 52036 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,19 +1,21 @@
 n = int(input())
-room_check = []
+room = []
 
 for i in range(n):
-    a,b = map(int, input().split())
-    room_check.append([a,b])
+    a = list(map(int, input().split()))
+    room.append(a)
 
-room_check.sort(key=lambda x:x[0])
-room_check.sort(key=lambda x:x[1])
 
-cnt = 1
-end = room_check[0][1]
+# 처음에는 끝나는 시간인 room[i][1]에 대해서만 정렬하면 된다고 생각했음.
+# 근데 회의 시작시간을 고려하지 않으면 3 3 / 2 3일 경우에 1로 나와버림
+room.sort(key=lambda x:x[0])
+room.sort(key=lambda x:x[1])
+
+end = room[0][1] # 맨 처음 회의가 끝나는 시간
+cnt = 1 # 회의는 최소 1개
 for i in range(1, n):
-    if end <= room_check[i][0]:
+    if end <= room[i][0]:
         cnt += 1
-        end = room_check[i][1]
-
+        end = room[i][1]
 
 print(cnt)

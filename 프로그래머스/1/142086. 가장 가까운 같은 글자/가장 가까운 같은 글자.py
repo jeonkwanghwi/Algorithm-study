acbb2cfecf4078ca35dda2d98f945fc002543e66
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2024-05-15 23:46:56 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.69 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,14 +1,12 @@
 def solution(s):
-    lst = []
     dict = {}
     ans = [0] * len(s)
 
     for i in range(len(s)):
         if s[i] not in dict:
-            dict[s[i]] = i
             ans[i] = -1
         else:
             ans[i] = i - dict[s[i]]
-            dict[s[i]] = i # 현재 인덱스로 업데이트
+        dict[s[i]] = i
 
     return ans

c64549df3a14300f65604da6f432255333a53968
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2024-05-27 16:57:07 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,11 +1,10 @@
-def factorial(n):
+def fibo(n):
     if n==0:
         return 0
-    elif n==1:
+    if n<=2:
         return 1
     else:
-        return factorial(n-2) + factorial(n-1)
+        return fibo(n-2) + fibo(n-1)
 
 n = int(input())
-
-print(factorial(n))
+print(fibo(n))

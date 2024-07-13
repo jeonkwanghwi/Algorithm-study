2454465c9e3f2e6b81dcfddfb6879f648d180646
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2024-07-13 18:48:35 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 544 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,6 +1,5 @@
 for _ in range(int(input())):
     lst = input().split()
     for i in range(len(lst)):
-        lst[i] = lst[i][::-1]
+        lst[i] = lst[i][::-1] # 각 원소별 순서 뒤집기
         print(lst[i], end=' ')
-    print()

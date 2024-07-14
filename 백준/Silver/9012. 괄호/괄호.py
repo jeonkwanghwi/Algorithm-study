a4d5ae732ef69e15e393972f80966aaac7a71adb
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2024-07-14 20:14:17 +0900

    [Silver IV] Title: 괄호, Time: 68 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -5,10 +5,14 @@ for _ in range(n):
     dq = deque(input())
     compare = 0
 
-    if dq.count("(") != dq.count(")"): # 개수 자체가 다르면 비교 X
+    if dq.count("(") != dq.count(")"): # 괄호의 개수 자체가 다르면 비교 X
         print("NO")
         continue
 
+    """
+    (은 )보다 항상 많거나 같아야 한다는 것을 이용
+    개수를 체크하면서 compare 값을 늘려서 음수가 된다면 ())와 같은 상황이므로 NO
+    """
     for i in dq:
         if i == "(":
             compare += 1
@@ -17,5 +21,5 @@ for _ in range(n):
         if compare < 0:
             print("NO")
             break
-    if compare >= 0:
+    if compare == 0:
         print("YES")

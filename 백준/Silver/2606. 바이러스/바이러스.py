cac2c9cfe74dca95b54131d888402607271950d3
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2024-08-15 02:32:39 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,24 +1,28 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(5000)
+
 n = int(input())
-v = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n + 1)
+count = 0 # 바이러스에 감염된 컴퓨터 개수
 
-# 경로를 저장하기 위한 2차원 리스트
-graph = [[] * n for _ in range(n + 1)]
+# 연결 리스트로 초기화하는 방법
+for i in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
-for _ in range(v):
-    a, b = map(int, input().split())
-    graph[a].append(b) # index와 값으로 서로를 연결해줌
-    graph[b].append(a) # 즉, 해당 인덱스에 값이 있으면 연결되어 있는 것.
-
-cnt = 0
-visited = [0] * (n + 1) # 모두 0으로 초기화, 0은 방문안한거고 1은 방문한것
-
-def dfs(start):
-    global cnt
-    visited[start] = 1 # 방문 체크
-    for i in graph[start]:
-        if visited[i] == 0: # 만약 방문 안했다면
+def dfs(v):
+    global count
+    count += 1
+    visited[v] = True
+    for i in graph[v]: # i와 연결된 노드중에 방문하지 않았다면 방문
+        if not visited[i]:
             dfs(i)
-            cnt += 1 # dfs 빠져나오고서, 방문한 노드 개수 하나 증가
 
-dfs(1) # 1번부터 시작
-print(cnt)
+
+dfs(1)
+
+print(count-1)

2e751188209d5369e536336bdfab722869f4350b
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2024-10-07 13:10:40 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,31 +1,25 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
 
 n, m, = map(int, input().split())
-maze = [[0] * m for _ in range(n)]
-for i in range(n):
-    tmp = input().rstrip()
-    for j in range(m):
-        maze[i][j] = int(tmp[j])
+graph = []
+visited = [[0] * m for _ in range(n)]
+for _ in range(n):
+    graph.append(list(input()))
 
 dx = [0,0,-1,1]
 dy = [1,-1,0,0]
 
-def BFS(x, y):
-    queue = deque()
-    queue.append((x,y))
+def BFS(a, b):
+    queue = deque() # 덱 초기화
+    queue.append((a,b)) # 초기값 append
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-
-            # 범위 내에서 이동하는 상황 & maze 벽이 아닌 경우(값이 0이 아닌 경우)
-            if 0 <= nx < n and 0 <= ny < m and maze[nx][ny] == 1:
-                maze[nx][ny] = maze[x][y] + 1 # 시간을 누적해가듯, 지나간 경로 횟수를 현재 위치에 할당
+            if 0 <= nx < n and 0 <= ny < m and int(graph[nx][ny])==1: # 범위 내고, 이동할 수 있는 위치라면 (그래프값이 1)
+                graph[nx][ny] = int(graph[x][y]) + 1
                 queue.append((nx, ny))
-    return maze[n-1][m-1]
-
+    return graph[n-1][m-1] # 누적하면서 끝을 도달하니깐 맨 마지막 위치의 값이 정답
 
 print(BFS(0,0))

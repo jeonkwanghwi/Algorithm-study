663e93db395256366373a161e4efec8e43f5d4ca
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2024-10-21 20:27:04 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,4 @@
+select ub.TITLE, ub.BOARD_ID, ur.REPLY_ID, ur.WRITER_ID, ur.CONTENTS, DATE_FORMAT(ur.CREATED_DATE, '%Y-%m-%d') as CREATED_DATE
+from USED_GOODS_BOARD as ub join USED_GOODS_REPLY as ur on ub.BOARD_ID=ur.BOARD_ID
+where ub.CREATED_DATE like '2022-10%'
+order by ur.CREATED_DATE, ub.TITLE
